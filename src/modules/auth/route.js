e5fc62f9192a1918ff6924b5dc24d806b0d0fa55
Muhammad Usama <46845901+usamaRehman1@users.noreply.github.com>
2026-07-09 23:04:14 +0500

    change protect route

--- src/modules/auth/route.js
+++ src/modules/auth/route.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import getController from "./controller/get.js";
 import signUpController from "./controller/signup.js";
 import logInController from "./controller/login.js"
-import { protect } from "../../middlewear/authMiddlewear.js";
+import { protect } from "../../middleware/authMiddleware.js";
 
 const router = Router();
 
