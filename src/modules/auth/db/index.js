9017bdc732332ae31f122026a63c9252575ff629
Muhammad Usama <46845901+usamaRehman1@users.noreply.github.com>
2026-07-09 22:48:37 +0500

    auth controller work done

---
+++ src/modules/auth/db/index.js
@@ -0,0 +1,54 @@
+import bcrypt from "bcryptjs";
+import { UserModel } from "../model/index.js";
+import { ENV } from "../../../constant/index.js";
+import { generateTokenJWT } from "../../../helperFunc/generateTokenJWT.js";
+
+const getData = async (data) => {
+    const { email, password } = data;
+    try {
+        const user = await UserModel.findOne({ email });
+        if (!user) {
+            throw new Error('Invalid Credentials');
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            throw new Error('Invalid Credentials');
+        }
+        const token = generateTokenJWT(user._id)
+        
+        return {
+            token,
+            user
+        }
+    } catch (error) {
+        throw new Error(error.message);
+    }
+}
+
+const addData = async (data) => {
+    try {
+        const { name, email, password } = data;
+        let userExists = await UserModel.findOne({ email });
+        if (userExists) {
+            throw new Error("User already exists");
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+        const user = await UserModel.create({ name, email, password: hashedPassword });
+        // const token = jwt.sign({ id: user._id }, ENV.JWT_SECRET, { expiresIn: '7d' });
+
+        return user
+    } catch (error) {
+        throw new Error(error.message);
+        // return false
+    }
+}
+
+const getAllUsers = async (condition) => await UserModel.find(condition);
+
+export {
+    addData,
+    getData,
+    getAllUsers,
+}
---
+++ src/modules/auth/route.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import getController from "./controller/get.js";
+import signUpController from "./controller/signup.js";
+import logInController from "./controller/login.js"
+import { protect } from "../../middlewear/authMiddlewear.js";
+
+const router = Router();
+
+router.get("/", protect, getController)
+router.post("/signup", signUpController)
+router.post("/login", logInController)
+
+export default router
